9a6a87290135d50e4ee7603a17330628aaad1eca
Blerta <115003968+blertaaqerimi@users.noreply.github.com>
2023-09-08 19:47:37 +0200

    added header and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,20 @@
 
 const Footer = () => {
-    return (
-      <div>
-          footer
-      </div>
-    )
-  }
-  
-  export default Footer;
+  return (
+    <div className="container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          <span className="mb-3 mb-md-0 text-body-secondary">© {new Date().getFullYear()} Recipes</span>
+        </div>
+
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="ms-3"><a className="text-body-secondary" href="www.twitter.com"><i class="bi bi-twitter"></i></a></li>
+          <li className="ms-3"><a className="text-body-secondary" href="www.instagram.com"><i class="bi bi-instagram"></i></a></li>
+          <li className="ms-3"><a className="text-body-secondary" href="www.facebook.com"><i class="bi bi-facebook"></i></a></li>
+        </ul>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,34 @@
+import { Link, useLocation } from "react-router-dom";
+import React from 'react';
+
+const routes = [
+  { path: "/", label: "Home" },
+  { path: "/recipes", label: "Recipes" },
+  { path: "/about", label: "About" },
+];
 
 const Header = () => {
-    return (
-      <div>
-          Header
-      </div>
-    )
-  }
-  
-  export default Header;
+  const location = useLocation();
+
+  return (
+    <div className="container">
+      <header className="d-flex justify-content-center py-3">
+        <ul className="nav nav-pills">
+          {routes.map((route, index) => (
+            <li className="nav-item" key={index}>
+              <Link
+                to={route.path}
+                className={`nav-link ${location.pathname === route.path ? 'active' : ''}`}
+                aria-current="page"
+              >
+                {route.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
